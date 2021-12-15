69154d8002623eec84b50890a19477b5f54cdb50
Rheaazarraga <rheaazarraga@gmail.com>
2021-12-14 21:43:25 -0800

    Insert filter method with a condition that the panels depend on this.state.focused value to focus on a single panel or all 4 panels if this.state.focus is null

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -33,7 +33,8 @@ class Dashboard extends Component {
 
 
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
   render() {
@@ -43,7 +44,12 @@ class Dashboard extends Component {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
+    const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+            
+      .map(panel => (
       <Panel
         key={panel.id}
         id={panel.id}
